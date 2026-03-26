c0d5d56af65dd6446574a2d7f59a023b02a9965e
Dryet <lolgg088@gmail.com>
2026-03-26 15:33:42 +0200

    git innit

--- MoneyAgregator/obj/Debug/net10.0/MoneyAgregator.AssemblyInfo.cs
+++ MoneyAgregator/obj/Debug/net10.0/MoneyAgregator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneyAgregator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21294b7625840dfd206582df0b425d32066a113c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneyAgregator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoneyAgregator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
