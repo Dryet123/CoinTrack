c30c6f5e62a6f673d16e9b1f81af5607a3ed1d45
Dryet <lolgg088@gmail.com>
2026-04-10 14:21:52 +0300

    feat Repository interface

---
+++ MoneyAgregator/Aplication/ICurrencyRepository.cs
@@ -0,0 +1,8 @@
+﻿using MoneyAgregator.Core.Entity;
+namespace MoneyAgregator.Aplication;
+
+public interface ICurrencyRepository : IRepository<Currency>
+{
+    Task<Currency> FindByCodeAsync(Guid id);
+    
+}
--- MoneyAgregator/Aplication/IRepository.cs
+++ MoneyAgregator/Aplication/IRepository.cs
@@ -1,6 +1,10 @@
 ﻿namespace MoneyAgregator.Aplication;
 
-public interface IRepository
+public interface IRepository<T> where T: class
 {
+    Task<List<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<T> AddAsync(T entity);
+    
     
 }
