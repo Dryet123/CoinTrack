014066092738494e03b4960c7e199fe9ed0468e2
Dryet <lolgg088@gmail.com>
2026-04-25 18:56:58 +0300

    rename

--- MoneyAgregator/Infrastructure/DbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MoneyAgregator.Infrastructure;
-using Microsoft.EntityFrameworkCore;
-using MoneyAgregator.Core.Entity;
-
-public class CurrencyDbContext : DbContext
-{
-    DbSet<CurrencyEntity> Currencies { get; set; }
-    
-}
