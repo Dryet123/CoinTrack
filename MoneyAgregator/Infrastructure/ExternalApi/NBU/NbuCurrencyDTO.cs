1946177f5e8813cebed3935a1f4bca31ac23b22e
Dryet <lolgg088@gmail.com>
2026-05-02 19:52:47 +0300

    Feat Added DTO for Nbu api

---
+++ MoneyAgregator/Infrastructure/ExternalApi/NBU/NbuCurrencyDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MoneyAgregator.Infrastructure.ExternalApi.NBU;
+
+public class NbuCurrencyDto
+{
+    [JsonPropertyName("r030")]
+    public int Code { get; set; }
+    [JsonPropertyName("сс")]
+    public string CurrencyCode { get;}
+    [JsonPropertyName("rate")]
+    public decimal Rate { get;}
+    [JsonPropertyName("exchangedate")]
+    public DateTime RateUpdateDate { get;}
+}
--- MoneyAgregator/obj/Debug/net10.0/MoneyAgregator.AssemblyInfo.cs
+++ MoneyAgregator/obj/Debug/net10.0/MoneyAgregator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneyAgregator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+398913b56610231534e8c8c603bae7e3a9f95c71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4cf224ef7e7b8af59e485d97e742f572c2a42d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneyAgregator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoneyAgregator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
