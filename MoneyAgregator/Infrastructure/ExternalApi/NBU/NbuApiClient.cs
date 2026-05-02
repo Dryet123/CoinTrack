372b2aaa4c09c5d4b2753e5bdfebdc05a1179d9e
Dryet <lolgg088@gmail.com>
2026-05-02 20:00:32 +0300

    Feat Added NbuApiClient.cs

---
+++ MoneyAgregator/Infrastructure/ExternalApi/NBU/NbuApiClient.cs
@@ -0,0 +1,22 @@
+﻿namespace MoneyAgregator.Infrastructure.ExternalApi.NBU;
+using MoneyAgregator.Infrastructure.ExternalApi.NBU;
+
+public class NbuApiClient
+{
+    readonly HttpClient _httpClient;
+    
+    public NbuApiClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<NbuCurrencyDto>> GetCurrencyAsync()
+    {
+        var responce = await _httpClient.GetFromJsonAsync<List<NbuCurrencyDto>>
+        (
+            "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+        );
+        return responce ?? new List<NbuCurrencyDto>();
+    }
+    
+}
