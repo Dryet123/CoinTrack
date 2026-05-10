b9a093773ad5c8934006810625e684814e53dcca
Dryet <lolgg088@gmail.com>
2026-05-10 19:56:17 +0300

    Upd: added new function TimeUpdatedAsync

--- MoneyAgregator/Core/Interfaces/ICurrencyRepository.cs
+++ MoneyAgregator/Core/Interfaces/ICurrencyRepository.cs
@@ -5,8 +5,11 @@ namespace MoneyAgregator.Core.Interfaces;
 public interface ICurrencyRepository : IRepository<CurrencyEntity>
 {
     Task<CurrencyEntity> FindByCodeAsync(string code);
+    
     Task<CurrencyEntity> FindByCurrencyCodeAsync(string currencycode);
     
     Task UpsertCurrenciesAsync(IEnumerable<CurrencyEntity> currencies);
+    
+    Task<DateTime> TimeUpdatedAsync();
     
 }
