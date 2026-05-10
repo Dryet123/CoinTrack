349e106d58d6efc089cb5b13b24a15ca2b3e62c3
Dryet <lolgg088@gmail.com>
2026-05-10 19:16:25 +0300

    fix CurencyEntity constructor deleted and setters are now public

--- MoneyAgregator/Core/Entity/Currency.cs
+++ MoneyAgregator/Core/Entity/Currency.cs
@@ -2,22 +2,12 @@
 
 public class CurrencyEntity
 {
-    public Guid Id { get; private  set; }
+    public Guid Id { get; set; }
     
-    public string Code { get; private  set; }
-    public string CurrencyCode { get; private set; }
+    public string Code { get;  set; }
+    public string CurrencyCode { get; set; }
     public decimal Rate { get; private set; }
     public DateTime RateUpdateDate { get; private set; }
-    
-    
-    public CurrencyEntity(string currencyCode,string code, decimal rate)
-    {
-       CurrencyCode = code;
-       Code = code;
-       Rate = rate;
-       RateUpdateDate = DateTime.UtcNow;
-       
-    }
     
     void UpdateRate(decimal newRate)
     {
