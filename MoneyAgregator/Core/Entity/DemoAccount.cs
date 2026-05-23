704e19b3ba177a9c3b335a71d6999f53df255c7e
Dryet <lolgg088@gmail.com>
2026-05-23 20:45:09 +0300

    Feat: Added new Entity

---
+++ MoneyAgregator/Core/Entity/DemoAccount.cs
@@ -0,0 +1,13 @@
+﻿namespace MoneyAgregator.Core.Entity;
+
+public class DemoAccount
+{
+    Guid Id { get; }
+    
+    string Code { get; }
+    
+    decimal Balance { get; }
+
+
+    private ICollection<DemoTransaction> Transactions = new List<DemoTransaction>();
+}
---
+++ MoneyAgregator/Core/Entity/DemoTransaction.cs
@@ -0,0 +1,17 @@
+﻿namespace MoneyAgregator.Core.Entity;
+
+public class DemoTransaction
+{
+    public Guid TransactionId;
+    
+    public Guid AccountId;
+    
+    public DemoAccount Account;
+    
+    public decimal Amount;
+    
+    public string Description;
+    
+    
+    
+}
